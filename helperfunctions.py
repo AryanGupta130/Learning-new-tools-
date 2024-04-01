6b186b6f51c0583b67930ec27a48c7060456c65f
amg7817 <amg7817@psu.edu>
2024-03-31 23:14:40 -0400

    created methods for showing an interactive graph for stock that is chosen

--- helperfunctions.py
+++ helperfunctions.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timedelta
+import plotly.graph_objs as go
 
+
+## these functions will show the different percent changes over a period of time
 def calculate_percentChange_24hours(data_array, symbol):
     percent_change = (data_array[0][4] - data_array[1][4]) / data_array[1][4] * 100
     Ticker = symbol.upper()
@@ -26,3 +29,29 @@ def calculate_percentChange_last9months(data_array, symbol):
 
 def calculate_percentChange_lastYear(data_array, symbol):
     return None
+
+
+## this function will be used for plotting purposes
+def plot_candlestick_chart(data_array, symbol):
+    # Extracting data for the candlestick chart
+    dates = data_array[:, 0]
+    opens = data_array[:, 1]
+    highs = data_array[:, 2]
+    lows = data_array[:, 3]
+    closes = data_array[:, 4]
+
+    # Create Candlestick chart
+    fig = go.Figure(data=[go.Candlestick(x=dates,
+                    open=opens,
+                    high=highs,
+                    low=lows,
+                    close=closes)])
+
+    # Update chart layout
+    fig.update_layout(title=f'Candlestick Chart for {symbol}',
+                      xaxis_title='Date',
+                      yaxis_title='Price')
+
+    # Show the chart
+    fig.show()
+
