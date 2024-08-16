96f58079b8934a257a5282c9bc31f4a7bde57133
Aryangupta130 <aryanguptaswas@gmail.com>
2024-08-16 01:44:05 -0400

    Developing the API for the user auth | Serialiser created

--- stock_backend/user_auth/models.py
+++ stock_backend/user_auth/models.py
@@ -1,30 +1,25 @@
 from django.db import models
 ## this library will allow me to import a basic model
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from rest_framework import serializers
 
 
 
-# Create your models here.
 
+## Creating this taking it from the deault data that is seen in the AbstractUser template
 class CustomUser(AbstractUser):
-    user_type = models.CharField(max_length=50, default='player')  # e.g., 'player', 'admin'
-
-    def __str__(self):
-        return self.username
+    pass
     
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'password', 'user_type']
+        fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password'],
-            user_type=validated_data.get('user_type', 'player')
+            password=validated_data['password']
         )
         return user
