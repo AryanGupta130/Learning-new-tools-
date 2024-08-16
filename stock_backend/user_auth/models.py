00a0fe02df0dd052d4ba13ad71fa08040bc16768
Aryangupta130 <aryanguptaswas@gmail.com>
2024-08-16 10:00:37 -0400

    I created the serialser.py for this logic. I have also created the view that allows users to register. Need to connect this to the URLS.py file.

--- stock_backend/user_auth/models.py
+++ stock_backend/user_auth/models.py
@@ -10,12 +10,16 @@ from rest_framework import serializers
 class CustomUser(AbstractUser):
     pass
     
+
+## Serialiser will allow me to convert data from different datatypes into JSON format
 class UserSerializer(serializers.ModelSerializer):
+    ## this inludes all of the fields that should be inluded when perfoming the serialisation
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
+    ## this will make it soo that this will help with creating a new user and adding their data into the database 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
--- stock_backend/user_auth/views.py
+++ stock_backend/user_auth/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+## the generics.CreateAPIView is a buitl in class that handles creating new model instances in a database
+class RegisterView(generics.CreateAPIView):
+    ## this is a list of all the queries that are being performed on the CustomUser model
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    ## anyone can access the registration page without thier being logged in
+    permission_classes = [AllowAny]
